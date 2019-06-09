1bb0c37c947938abb17dde80589500f94f270f7c
William Kwok <wkwok16@uw.edu>
2019-06-09 08:39:17 -0700

    Added theme color tag to helmet content

--- components/Page/HelmetContent/HelmetContent.tsx
+++ components/Page/HelmetContent/HelmetContent.tsx
@@ -18,6 +18,7 @@ const icon = require('../../_assets/site/icon.png');
 export const HelmetContent = () => {
     return <Helmet>
         <meta charSet="utf-8" />
+        <meta name="theme-color" content="#39275b"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/** OpenGraph content. Used for when our page is linked to other websites like Facebook or Slack */}
