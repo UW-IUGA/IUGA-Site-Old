1fcebe6eb50c9506093b0f909c72057117d8d0fa
William Kwok <wkwok16@uw.edu>
2019-07-06 18:59:00 -0700

    Set up base for pastofficers page

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import next from 'next';
+import { CURRENT_YEAR } from 'components/_data/constantsToChangeEachYear';
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production'
@@ -20,13 +21,13 @@ const handle = app.getRequestHandler();
  */
 app.prepare()
     .then(() => {
-        const server = express()
+        const server = express();
 
         /** 
          * This is the elections endpoint that provides the year as a parameter
          */
         server.get('/elections/:year', (req: Request, res: Response) => {
-            return app.render(req, res, '/elections', { year: req.params.year });
+            return app.render(req, res, '/elections', { year: req.params.year || CURRENT_YEAR });
         })
 
         server.get('*', (req: Request, res: Response) => {
--- pages/pastofficers.tsx
+++ pages/pastofficers.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { pastOfficersData } from '../components/_data/pastOfficersData';
+import { CURRENT_YEAR } from 'components/_data/constantsToChangeEachYear';
+
 
 const PastOfficers: React.FC = () => {
+    const [selectedYear, setSelectedYear] = useState(CURRENT_YEAR);
+
+    let orderedYearsNewToOld = Object.keys(pastOfficersData).sort((a, b) => {
+        return Number(b) - Number(a);
+    });
+
+    let orderedYearsOldToNew = Object.keys(pastOfficersData).sort((a, b) => {
+        return Number(a) - Number(b);
+    })
+
     return <>
         This page is under construction
     </>
